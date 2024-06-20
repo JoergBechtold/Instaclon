29cf0fb5a24bca4dab2d7814d6afd58cfbe7438e
Jörg Bechtold <joergbechtold@gmx.net>
2024-06-20 12:12:37 +0200

    comments works finaly

--- script/main.js
+++ script/main.js
@@ -9,9 +9,9 @@ let posts = [
     postImg: 'img/badesee.jpg',
     postText: `Europäische Umweltagentur: Wasserqualität deutscher Badegewässer weiter sehr gut Fast alle deutschen Badegewässer erfüllen die
               Mindestanforderungen der Europäischen Umweltagentur. Nur an sieben der mehr als 2.000 Gewässer wurden Mängel festgestellt.`,
-    commentProfile: 'Jens Rödder',
+    commentProfile: ['Jens Rödder'],
     comments: ['Außer bei unserem See um die Ecke leider nicht :('],
-    commentTime: 14,
+    commentTime: [14],
     isLiked: true,
     likes: 1,
   },
@@ -21,11 +21,12 @@ let posts = [
     postImg: 'img/american-football.jpg',
     postText: `Spannend bis zum Ende - erst gehen die @hof.jokers früh im ersten Quarter in Führung, dann drehen unsere Specialteams das Spiel mit 4 Field Goals.
                Am Ende hält die Defense und sichert den dritten Sieg mit einer Interception.`,
-    commentProfile: 'Max Rothe',
+    commentProfile: ['Max Rothe', 'AndySchmitt'],
     comments: [
       'Mega geil! Freue mich so derbe über euren Sieg gegen Hof wurde längst mal wieder zeit! Alles Gute für die weitere Saison lassts krachen! Schelle und Punkte!',
+      'Eine Wahnsinns Stimmung im Stadion',
     ],
-    commentTime: 1,
+    commentTime: [4, 1],
     isLiked: true,
     likes: 23,
   },
@@ -34,9 +35,9 @@ let posts = [
     author: 'Lewe MG',
     postImg: 'img/post-picture.jpg',
     postText: `Ich war im Urlaub im Monument Valley, einem der beeindruckendsten Orte, die ich je gesehen habe. Die roten Felsformationen und die weiten, offenen Landschaften sind wirklich atemberaubend. Ich habe Sonnenauf- und -untergänge beobachtet, die den Himmel in lebendige Farben tauchten, und die Stille und Schönheit der Natur genossen. Es war eine unvergessliche Reise, die mir noch lange in Erinnerung bleiben wird.`,
-    commentProfile: ['DanielEggers'],
-    comments: ['Das klingt absolut fantastisch! Monument Valley muss wirklich ein magischer Ort sein.'],
-    commentTime: 3,
+    commentProfile: ['DanielDer5.', 'Ramon Deggers'],
+    comments: ['Das klingt absolut fantastisch! Monument Valley muss wirklich ein magischer Ort sein.', 'Ich war 2018 da. Einfch wunderschön'],
+    commentTime: [12, 10],
     isLiked: true,
     likes: 7,
   },
@@ -89,76 +90,63 @@ function showPostContent() {
           </span>
         </div>
     
-        <div  class="comments-section">
-         
-        <div id="generated_new_comment_container${i}" class="current-comments d-flex-column padding-left-right">
+        <div  class="comments-section"> 
+          <div id="generated_new_comment_container${i}" class="current-comments d-flex-column padding-left-right">
             <!-- Generate Comments -->
-            <div class="d-flex-just-cont-space-between">
-            <div class="comment-profile-name-time">
-              <div class="d-flex-column">
-                <a class="comment-profile" href="#"><nobr>${post['commentProfile']}</nobr></a>
-                <p" class="comment-time"><nobr>Vor ${post['commentTime']} Stunden</nobr></p>
-              </div>
-    
-              <div class="comment-profile-text">
-                <span>
-                  ${post['comments']}
-                </span>
-              </div>
-            </div>
-    
-            <div class="comment-heart-icon">
-              <img src="img/icon-heart.svg" alt="herz icon" />
-            </div>
           </div>
         </div>
-           <!-- Generate Comments -->
+           
         
           <div class="add-new-comment padding-left-right">
             <input class="add-new-comment-input" type="search" name="add-new-comment-input" id="comment_input${i}" placeholder="Kommentar hinzufügen" />
-            <button onclick="addNewComent(${i})" class="add-new-comment-button">Posten</button>
+            <button onclick="addNewComment(${i})" class="add-new-comment-button">Posten</button>
           </div>
         </div> 
       </div>
       </div>
       `;
+
+    let generateNewCommentContainer = document.getElementById(`generated_new_comment_container${i}`);
+
+    for (let j = 0; j < post['comments'].length; j++) {
+      let comment = post['comments'][j];
+      let commentProfile = post['commentProfile'][j];
+      let commentTime = post['commentTime'][j];
+      generateNewCommentContainer.innerHTML += `
+      <div class="d-flex-just-cont-space-between">
+        <div class="comment-profile-name-time">
+          <div class="d-flex-column">
+            <a class="comment-profile" href="#"><nobr>${commentProfile}</nobr></a>
+            <p" class="comment-time"><nobr>Vor ${commentTime} Stunden</nobr></p>
+          </div>
+
+          <div class="comment-profile-text">
+            <span>
+              ${comment}
+            </span>
+          </div>
+        </div>
+
+        <div class="comment-heart-icon">
+          <img src="img/icon-heart.svg" alt="herz icon" />
+        </div>
+
+         <div class="delete-icon">
+          <img src="img/xmark-search.svg" alt="Löschen" />
+        </div>
+
+        
+      </div>
+        `;
+    }
   }
 }
 
 /* Button new comment */
-function addNewComent(index) {
+function addNewComment(index) {
   let commentInput = document.getElementById(`comment_input${index}`);
   posts[index]['comments'].push(commentInput.value);
-  generateNewCommentContainer(index);
-  commentInput.value = '';
-  // showPostContent();
-}
-
-function generateNewCommentContainer(index) {
-  let generateNewCommentContainer = document.getElementById(`generated_new_comment_container${index}`);
-  generateNewCommentContainer.innerHTML = '';
-
-  for (let j = 0; j < posts[index]['comments'].length; j++) {
-    let newComment = posts[index]['comments'][j];
-    generateNewCommentContainer.innerHTML += `
- <div class="d-flex-just-cont-space-between">
-          <div class="comment-profile-name-time">
-            <div class="d-flex-column">
-              <a class="comment-profile" href="#"><nobr>${myProfileName}</nobr></a>
-              <p" class="comment-time"><nobr>Vor ${posts['commentTime']} Stunden</nobr></p>
-            </div>
-  
-            <div class="comment-profile-text">
-              <span>
-                ${newComment}
-              </span>
-            </div>
-          </div>
-  
-          <div class="comment-heart-icon">
-            <img src="/img/icon-heart.svg" alt="herz icon" />
-          </div>
-   </div>
-`;
-  }
+  posts[index]['commentProfile'].push(myProfileName);
+  posts[index]['commentTime'].push(1);
+  showPostContent();
 }
